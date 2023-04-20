734bb5c09cec1006ca0000daeef4d4220b2b63a4
mokuzaru <kenny.otoya@pucp.pe>
2023-04-19 22:11:12 -0500

    camera + death

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-     public Transform target;
+    public Transform target;
     public float cam_speed = 0.1f;
     Camera mycam;
-
+    //componente camera en y no menor a -1
+    private Transform camara_pos;
     void Start()
     {
-        mycam = GetComponent<Camera>();    
+        mycam = GetComponent<Camera>(); 
+        camara_pos = GetComponent<Transform>();   
     }
 
 
     void Update()
     {
+        
+        
         mycam.orthographicSize = (Screen.height / 100f) / 1.2f;
         Vector3 newPos = new Vector3 (target.position.x , target.position.y, -10);
         if(target){
-            transform.position = Vector3.Lerp(transform.position, newPos, cam_speed);
+            transform.position = Vector3.Lerp(new Vector3(transform.position.x, 0.0f,transform.position.z), newPos, cam_speed);
         }        
     }
 }
