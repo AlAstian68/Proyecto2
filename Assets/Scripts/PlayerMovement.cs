d2c1ca3c0d179130eda3f96698c730719ec2df87
mokuzaru <kenny.otoya@pucp.pe>
2023-04-21 21:13:19 -0500

    merge

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {    
-    private Rigidbody2D rigid;
+    private Rigidbody2D rb2D;
     private float h;
-    public float vel;
-    public float jumpSpeed = 30;
+    //public float vel;
+    //public float jumpSpeed = 30;
+    public float runSpeed = 2;
+    public float jumpSpeed = 3;
     private Animator anim;
     //for shoting
-    bool lado = false;
-    bool jump = false;
+    //bool lado = false;
+    //bool jump = false;
     
     public bool betterJummp = false;
     public float fallMultiplier = 0.5f;
     public float lowJumpMultiplier = 1f;
+
+    public SpriteRenderer spriteRenderer;
 
     //en-tierra
     const float player_size = 0.2f;
@@ -28,41 +32,66 @@ public class PlayerMovement : MonoBehaviour
   
     void Start()
     {
-        rigid = GetComponent<Rigidbody2D>();
+        rb2D = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        jump = false;
+     
     }
 
     void FixedUpdate()
     {
-        h = Input.GetAxis("Horizontal");
-        if(h < 0 && !lado){
-            lado = !lado;
-            transform.Rotate(0f,180f,0f);
-        }else if (h > 0 && lado){
-            lado = !lado;
-            transform.Rotate(0f,180f,0f);
+        if(Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2D.velocity = new Vector2(runSpeed, rb2D.velocity.y);
+            spriteRenderer.flipX = false;
+        }
+        else if(Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2D.velocity = new Vector2(-runSpeed, rb2D.velocity.y);
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+        }
+        
+        if (Input.GetKey("space") && CheckGround.isGrounded)
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+        }
+
+        if(betterJummp)
+        {
+            if(rb2D.velocity.y<0)
+            {
+                rb2D.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
+            }
+
+            if(rb2D.velocity.y>0 && !Input.GetKey("space"))
+            {
+                rb2D.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
+            }
+
         }
         
         anim.SetFloat("Player1Run",h);
-        rigid.MovePosition(rigid.position + new Vector2(h,0) * vel * Time.deltaTime);
+        //rb2D.MovePosition(rb2D.position + new Vector2(h,0) * runSpeed * Time.deltaTime);
 
-        esta_en_tierra();
+        /*esta_en_tierra();
         if(Input.GetButton("Jump") && !jump){
             jump = true;
-            rigid.AddForce(new Vector2(0f, jumpSpeed));
-
-        }
+            rb2D.AddForce(new Vector2(0f, jumpSpeed));
+        
+        }*/
         /*
         if (betterJummp)
         {
-            if (rigid.velocity.y < 0)
+            if (rb2D.velocity.y < 0)
             {
-                rigid.velocity += Vector2.up*Physics2D.gravity.y*(fallMultiplier)*Time.deltaTime;
+                rb2D.velocity += Vector2.up*Physics2D.gravity.y*(fallMultiplier)*Time.deltaTime;
             }
-            if (rigid.velocity.y > 0 && !Input.GetKey("space"))
+            if (rb2D.velocity.y > 0 && !Input.GetKey("space"))
             {
-                rigid.velocity += Vector2.up*Physics2D.gravity.y*(lowJumpMultiplier)*Time.deltaTime;
+                rb2D.velocity += Vector2.up*Physics2D.gravity.y*(lowJumpMultiplier)*Time.deltaTime;
             }
         }
 */
@@ -83,8 +112,7 @@ public class PlayerMovement : MonoBehaviour
         //anim.SetBool("disparando",false);
     }
 
-    void esta_en_tierra(){
-
+    /*void esta_en_tierra(){
         Collider2D[] colliders = Physics2D.OverlapCircleAll(tierra_verificada.position, player_size,esto_tierra);
         for(int i = 0; i < colliders.Length; i++){
             if(colliders[i].gameObject != gameObject){
@@ -94,7 +122,7 @@ public class PlayerMovement : MonoBehaviour
         }
         
     }
-
+*/
 /*
      void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.tag == "enemy"){
